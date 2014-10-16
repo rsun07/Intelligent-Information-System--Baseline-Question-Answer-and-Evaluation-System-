897dc9316ebc1c863209897a6e9e7eb516124246
rsun07 <rsun2014@gmail.com>
2014-10-16 11:19:11 -0400

    initial

---
+++ hw3-xiaomins/src/main/java/edu/cmu/lti/f14/hw3/hw3_xiaomins/VectorSpaceRetrieval.java
@@ -0,0 +1,78 @@
+/**
+ * OBJECTIVE
+ *   Implement a vector space retrieval system using a UIMA framework
+ *   
+ * 
+ * DESCRIPTION
+ * This is the main function of this retrieval system.
+ * The analysis engine is SentenceAnalysis
+ * It composed of three analysis components, i.e.
+ * 1. DocumentReader  :: identify the query id and relevant scores
+ * 2. DocumentVectorAnnotator    :: identify the word and word frequency in each sentence
+ * 3. RetrievalEvaluator :: compute the similarity and performance metric of the collection 
+ * 
+ * 
+ * PROCEDURE 
+ * This system is fully functional but it is not working correctly.
+ * The input text collection is available at "dat_tiny".
+ * Your tasks are
+ * 0. Correctly extract bag of words feature vector from the input text collection
+ * 1. Compute the cosine similarity between two sentences in the text collection
+ * 2. Compute the Mean Reciprocal Rank (metric) for all sentences in the text collection
+ * 3. (optional) Improve the efficiency of the program and reduce the runtime of the retrieval system
+ * 
+ */
+package edu.cmu.lti.f14.hw3.hw3_xiaomins;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.util.XMLInputSource;
+
+public class VectorSpaceRetrieval {
+	
+	public static void main(String [] args) 
+			throws Exception {
+			
+		String sLine;
+		long startTime=System.currentTimeMillis();
+		
+		URL descUrl = VectorSpaceRetrieval.class.getResource("/descriptors/retrievalsystem/VectorSpaceRetrieval.xml");
+	   if (descUrl == null) {
+	      throw new IllegalArgumentException("Error opening VectorSpaceRetrieval.xml");
+	   }
+		// create AnalysisEngine		
+		XMLInputSource input = new XMLInputSource(descUrl);
+		AnalysisEngineDescription desc = UIMAFramework.getXMLParser().parseAnalysisEngineDescription(input);
+		AnalysisEngine anAnalysisEngine = UIMAFramework.produceAnalysisEngine(desc);
+		CAS aCas = anAnalysisEngine.newCAS();
+
+	  URL docUrl = VectorSpaceRetrieval.class.getResource("/data/documents.txt");
+    if (docUrl == null) {
+       throw new IllegalArgumentException("Error opening data/documents.txt");
+    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(docUrl.openStream()));
+		while ((sLine = br.readLine()) != null)   {
+			aCas.setDocumentText(sLine);
+			anAnalysisEngine.process(aCas);
+			aCas.reset();
+		}
+		br.close();
+		br=null;
+		anAnalysisEngine.collectionProcessComplete();
+		anAnalysisEngine.destroy();	
+		long endTime=System.currentTimeMillis();
+		
+		double totalTime=(endTime-startTime)/1000.0;
+		System.out.println("Total time taken: "+totalTime);
+		
+
+	}
+
+}
---
+++ hw3-xiaomins/src/main/java/edu/cmu/lti/f14/hw3/hw3_xiaomins/typesystems/Token.java
@@ -0,0 +1,101 @@
+
+
+/* First created by JCasGen Fri Oct 11 01:58:03 EDT 2013 */
+package edu.cmu.lti.f14.hw3.hw3_xiaomins.typesystems;
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** 
+ * Updated by JCasGen Fri Oct 11 01:58:03 EDT 2013
+ * XML source: /home/diwang/ur-workspace/hw3-xiaomins/src/main/resources/descriptors/typesystems/VectorSpaceTypes.xml
+ * @generated */
+public class Token extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(Token.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated  */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected Token() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated */
+  public Token(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated */
+  public Token(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated */  
+  public Token(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** <!-- begin-user-doc -->
+    * Write your own initialization here
+    * <!-- end-user-doc -->
+  @generated modifiable */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: text
+
+  /** getter for text - gets 
+   * @generated */
+  public String getText() {
+    if (Token_Type.featOkTst && ((Token_Type)jcasType).casFeat_text == null)
+      jcasType.jcas.throwFeatMissing("text", "edu.cmu.lti.f14.hw3.hw3_xiaomins.typesystems.Token");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((Token_Type)jcasType).casFeatCode_text);}
+    
+  /** setter for text - sets  
+   * @generated */
+  public void setText(String v) {
+    if (Token_Type.featOkTst && ((Token_Type)jcasType).casFeat_text == null)
+      jcasType.jcas.throwFeatMissing("text", "edu.cmu.lti.f14.hw3.hw3_xiaomins.typesystems.Token");
+    jcasType.ll_cas.ll_setStringValue(addr, ((Token_Type)jcasType).casFeatCode_text, v);}    
+   
+    
+  //*--------------*
+  //* Feature: frequency
+
+  /** getter for frequency - gets 
+   * @generated */
+  public int getFrequency() {
+    if (Token_Type.featOkTst && ((Token_Type)jcasType).casFeat_frequency == null)
+      jcasType.jcas.throwFeatMissing("frequency", "edu.cmu.lti.f14.hw3.hw3_xiaomins.typesystems.Token");
+    return jcasType.ll_cas.ll_getIntValue(addr, ((Token_Type)jcasType).casFeatCode_frequency);}
+    
+  /** setter for frequency - sets  
+   * @generated */
+  public void setFrequency(int v) {
+    if (Token_Type.featOkTst && ((Token_Type)jcasType).casFeat_frequency == null)
+      jcasType.jcas.throwFeatMissing("frequency", "edu.cmu.lti.f14.hw3.hw3_xiaomins.typesystems.Token");
+    jcasType.ll_cas.ll_setIntValue(addr, ((Token_Type)jcasType).casFeatCode_frequency, v);}    
+  }
+
+    
